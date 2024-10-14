5be9d98b1c1be3d9c95e108e4f0b0c1dc8862e16
gilfy <31819039+gilfy@users.noreply.github.com>
2024-10-14 13:55:59 +0200

    Add files via upload

---
+++ tsconfig.js
@@ -0,0 +1,14 @@
+{
+    "compilerOptions": {
+      "target": "es5",
+      "module": "commonjs",
+      "strict": true,
+      "jsx": "preserve",
+      "moduleResolution": "node",
+      "esModuleInterop": true,
+      "skipLibCheck": true,
+      "forceConsistentCasingInFileNames": true
+    },
+    "include": ["next-env.d.ts", "**/*.ts", "**/*.tsx"],
+    "exclude": ["node_modules"]
+  }
